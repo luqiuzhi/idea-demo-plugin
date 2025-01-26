e13ff1fc2ea25f6368e49cd8ffa859914680cfd3
GitHub Action <action@github.com>
2025-01-26 03:30:37 +0000

    Template cleanup

--- src/main/kotlin/com/github/luqiuzhi/ideademoplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/luqiuzhi/ideademoplugin/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.luqiuzhi.ideademoplugin.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.luqiuzhi.ideademoplugin.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
